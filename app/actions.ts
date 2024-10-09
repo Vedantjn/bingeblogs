3e0d4cb69055671c1d3d2b82b07800ef928836b1
Vedantjn <vedant.jain.indore@gmail.com>
2024-10-09 16:12:09 +0530

    code updated

--- app/actions.ts
+++ app/actions.ts
@@ -230,11 +230,11 @@ export async function CreateSubscription() {
     },
     success_url:
       process.env.NODE_ENV === "production"
-        ? "https://blog-marshal.vercel.app/dashboard/payment/success"
+        ? "https://bingeblogs.vercel.app/dashboard/payment/success"
         : "http://localhost:3000/dashboard/payment/success",
     cancel_url:
       process.env.NODE_ENV === "production"
-        ? "https://blog-marshal.vercel.app/dashboard/payment/cancelled"
+        ? "https://bingeblogs.vercel.app/dashboard/payment/cancelled"
         : "http://localhost:3000/dashboard/payment/cancelled",
   });
 
